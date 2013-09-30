91ba177636ea9c3bce6a141a5179a510035fce3f
jzfried <Jonathan@Jonathans-MacBook-Pro-2.local>
2013-09-30 10:54:19 +0300

    V2
    Application.js file updated

--- application.js
+++ application.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	$('#child1').mouseenter(function(){
-        $(this).prepend('img src="images/arrow-left.png" alt="highrise"');
-                .prop('src', 'images/arrow-left.png');
-                .css('position', 'relative');
-                .css('right', '10px');
+        $(this).prepend('img src="images/arrow-left.png" alt="highrise"')
+                .prop('src', 'images/arrow-left.png')
+                .css('position', 'relative')
+                .css('right', '10px')
                 .css('top', '10px');
 });
