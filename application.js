f96459adb707e95bd8904a5eceee44907fff0309
jzfried <Jonathan@Jonathans-MacBook-Pro-2.local>
2013-10-05 16:24:04 +0300

    V4
    Need to figure out how to style arrows

--- application.js
+++ application.js
@@ -1,31 +1,52 @@
 $(document).ready(function(){
-	$('#child1').mouseenter(function(){
-        var arrowImage = $(this).prepend('img src="images/arrow-left.png" alt="highrise"')
-                .prop('src', 'images/arrow-left.png')
-                .css('position', 'relative')
-                .css('right', '10px')
-                .css('top', '10px');
-        $(this).prepend(<h1>Basecamp is the project management tool you wish you had on your last project.</h1>);
-        $(this).prepend(<h3>Are you still managing projects with email? Are you still using Excel for your to-do lists? It's time to upgrade to Basecamp. Manage projects and collaborate with your team and clients the modern way.</h3>);
-});
-});
 
-	$('#child1').mouseleave(function(){
-		$(this).prepend(remove);
-	};
+    var arrowImageRight = $('<img src="images/arrow-left.png" alt="highrise">');
 
-$(document).ready(function(){
-	$('#child2').mouseenter(function(){
-        $(arrowImage);
-});
+    $('#child1').mouseenter(function(){
+        $(this).append(arrowImageRight);
+        $('arrowImageRight').addClass('arrowImageRight');
+        $('h1').replaceWith("<h2>Basecamp is the project management tool you wish you had on your last project.</h2>");
+        $('h3').replaceWith("<h4>Are you still managing projects with email? Are you still using Excel for your to-do lists? It's time to upgrade to Basecamp. Manage projects and collaborate with your team and clients the modern way.</h4>");
+    });
+
+    $('#child1').mouseleave(function(){
+        arrowImageRight.remove();
+        $('h2').replaceWith("<h1>Making collaboration productive and enjoyable for people every day</h1>")
+        $('h4').replaceWith("<h3>Frustration-free web-based apps for collaboration, sharing information, and making decisions.</h3>");
+    });
 });
 
 $(document).ready(function(){
-	$('#child3').mouseenter(function(){
-        $(this).prepend('img src="images/arrow-right.png" alt="highrise"')
-                .prop('src', 'images/arrow-right.png')
-                .css('position', 'relative')
-                .css('right', '10px')
-                .css('top', '10px');
+
+	var arrowImageRight = $('<img src="images/arrow-left.png" alt="highrise">');
+
+    $('#child2').mouseenter(function(){
+        $(this).append(arrowImageRight);
+        $('arrowImageRight').addClass('arrowImageRight');
+        $('h1').replaceWith("<h2>Highrise remembers the important things about people you'd normally forget.</h2>");
+        $('h3').replaceWith("<h4>Keep a permanent record of people you do business with. Know who you talked to, when you talked to them, what was said, and when to follow up next. Over 20,000,000 contacts are tracked using Highrise.</h4>");
+    });
+
+    $('#child2').mouseleave(function(){
+        arrowImageRight.remove();
+        $('h2').replaceWith("<h1>Making collaboration productive and enjoyable for people every day</h1>")
+        $('h4').replaceWith("<h3>Frustration-free web-based apps for collaboration, sharing information, and making decisions.</h3>");
+    });
 });
+
+$(document).ready(function(){
+
+	var arrowImageLeft = $('<img src="images/arrow-right.png" alt="highrise">');
+
+    $('#child3').mouseenter(function(){
+        $(this).append(arrowImageLeft);
+        $('h1').replaceWith("<h2>Campfire helps teams work together over the web in real-time.</h2>");
+        $('h3').replaceWith("<h4>Share ideas, discussions, concepts, images, code samples, videos, mockups, and documents in a real-time private chat room. It's game changing. We couldn't run our own business without Campfire.</h4>");
+    });
+
+    $('#child3').mouseleave(function(){
+        arrowImageLeft.remove();
+        $('h2').replaceWith("<h1>Making collaboration productive and enjoyable for people every day</h1>")
+        $('h4').replaceWith("<h3>Frustration-free web-based apps for collaboration, sharing information, and making decisions.</h3>");
+    });
 });
