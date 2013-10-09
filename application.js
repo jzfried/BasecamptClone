473dbe3e5ab742c44af8bdfcd2675ccfb97b628d
jzfried <Jonathan@Jonathans-MacBook-Pro-2.local>
2013-10-09 23:30:17 +0300

    Final version

--- application.js
+++ application.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
     $('#child1').mouseenter(function(){
         $(this).append(arrowImageRight);
-        $('arrowImageRight').addClass('arrowImageRight');
+        arrowImageRight.addClass('arrowImageRight')
         $('h1').replaceWith("<h2>Basecamp is the project management tool you wish you had on your last project.</h2>");
         $('h3').replaceWith("<h4>Are you still managing projects with email? Are you still using Excel for your to-do lists? It's time to upgrade to Basecamp. Manage projects and collaborate with your team and clients the modern way.</h4>");
     });
@@ -22,7 +22,7 @@ $(document).ready(function(){
 
     $('#child2').mouseenter(function(){
         $(this).append(arrowImageRight);
-        $('arrowImageRight').addClass('arrowImageRight');
+        arrowImageRight.addClass('arrowImageRight')
         $('h1').replaceWith("<h2>Highrise remembers the important things about people you'd normally forget.</h2>");
         $('h3').replaceWith("<h4>Keep a permanent record of people you do business with. Know who you talked to, when you talked to them, what was said, and when to follow up next. Over 20,000,000 contacts are tracked using Highrise.</h4>");
     });
@@ -40,6 +40,7 @@ $(document).ready(function(){
 
     $('#child3').mouseenter(function(){
         $(this).append(arrowImageLeft);
+        arrowImageLeft.addClass('arrowImageRight')
         $('h1').replaceWith("<h2>Campfire helps teams work together over the web in real-time.</h2>");
         $('h3').replaceWith("<h4>Share ideas, discussions, concepts, images, code samples, videos, mockups, and documents in a real-time private chat room. It's game changing. We couldn't run our own business without Campfire.</h4>");
     });
